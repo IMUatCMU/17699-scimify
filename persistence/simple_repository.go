cc64cd2080eac1af97e284dfffedf7805d123fd1
Weinan Qiu <davidiamyou@gmail.com>
2017-01-31 20:14:00 -0500

    Add simple in memory repo to create, delete and get SCIM resource by id

---
+++ persistence/simple_repository.go
@@ -0,0 +1,37 @@
+package persistence
+
+import "github.com/go-scim/scimify/resource"
+
+// An implementation of Repository interface that stores SCIM resource
+// in a hash map. It only offers functionality to create, delete and get SCIM
+// resource by id.
+type SimpleRepository struct {
+	repo	map[string]*resource.Resource
+}
+
+// Put the provided resource into the hash map, overwrites existing id
+func (r *SimpleRepository) Create(resource *resource.Resource) error {
+	r.repo[resource.Id] = resource
+	return nil
+}
+
+// Get the resource indexed by id in the hash map
+func (r *SimpleRepository) Get(id string) (*resource.Resource, error) {
+	return r.repo[id], nil
+}
+
+// Not implemented by design
+func (r *SimpleRepository) Replace(id string, resource *resource.Resource) error {
+	return nil
+}
+
+// Overwrite the indexed id slot with nil
+func (r *SimpleRepository) Delete(id string) error {
+	r.repo[id] = nil
+	return nil
+}
+
+// Not implemented by design
+func (r *SimpleRepository) Query(filter string, sortBy string, ascending bool, pageStart int32, pageSize int32) ([]*resource.Resource, error) {
+	return nil, nil
+}
