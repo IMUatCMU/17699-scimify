d29130d973c7a607fbd6653edb2f5439b04c74a0
Weinan Qiu <davidiamyou@gmail.com>
2017-02-15 19:37:43 -0500

    Added ability to concurrently traverse a slice and aggregate results

--- helper/parallel.go
+++ helper/parallel.go
@@ -5,6 +5,7 @@ import (
 )
 
 type MapEntryProcessor func(key string, value interface{}) (interface{}, error)
+type SliceElementProcessor func(idx int, elem interface{}) (interface{}, error)
 
 type Aggregator interface {
 	Aggregate(interface{})
@@ -14,6 +15,62 @@ type Aggregator interface {
 type result struct {
 	Value	interface{}
 	Err 	error
+}
+
+func WalkSliceInParallel(target []interface{}, processFunc SliceElementProcessor, aggregator Aggregator) (interface{}, error) {
+	done := make(chan struct{})
+	defer close(done)
+
+	c := processSliceElement(done, target, processFunc)
+	for r := range c {
+		if r.Err != nil {
+			return nil, r.Err
+		} else {
+			aggregator.Aggregate(r.Value)
+		}
+	}
+
+	return aggregator.Result(), nil
+}
+
+func processSliceElement(done <-chan struct{}, target []interface{}, processFunc SliceElementProcessor) (<-chan result) {
+	c := make(chan result)
+
+	go func() {
+		var wg sync.WaitGroup
+
+		// walk map
+		for i, e := range target {
+			index, element := i, e
+			wg.Add(1)
+
+			// walk entry
+			go func() {
+				r, err := processFunc(index, element)
+				select {
+				case c <- result{r, err}:
+				case <-done:
+				}
+				wg.Done()
+			}()
+
+			// abort walk if done
+			select {
+			case <-done:
+				return
+			default:
+				continue
+			}
+		}
+
+		// close result channel when everything is done
+		go func() {
+			wg.Wait()
+			close(c)
+		}()
+	}()
+
+	return c
 }
 
 func WalkStringMapInParallel(target map[string]interface{}, processFunc MapEntryProcessor, aggregator Aggregator) (interface{}, error) {
--- helper/parallel_test.go
+++ helper/parallel_test.go
@@ -20,6 +20,62 @@ func (g *testMapAggregator) Result() interface{} {
 	return "{" + strings.Join(g.state, ",") + "}"
 }
 
+type testSliceAggregator struct {
+	state	[]string
+}
+func (g *testSliceAggregator) Aggregate(input interface{}) {
+	if nil == g.state {
+		g.state = make([]string, 0)
+	}
+	g.state = append(g.state, input.(string))
+}
+func (g *testSliceAggregator) Result() interface{} {
+	return "[" + strings.Join(g.state, ",") + "]"
+}
+
+func TestWalkSliceInParallel(t *testing.T) {
+	for _, test := range []struct{
+		name 		string
+		target 		[]interface{}
+		processFunc	SliceElementProcessor
+		aggregator	Aggregator
+		assertion	func(interface{}, error)
+	}{
+		{
+			"process simple slice",
+			[]interface{}{"1", "2", "3"},
+			func(_ int, elem interface{}) (interface{}, error) {
+				return elem, nil
+			},
+			&testSliceAggregator{state:make([]string, 0)},
+			func(result interface{}, err error) {
+				assert.Nil(t, err)
+				assert.True(t, result == "[1,2,3]" ||
+					result == "[1,3,2]" ||
+					result == "[2,1,3]" ||
+					result == "[2,3,1]" ||
+					result == "[3,1,2]" ||
+					result == "[3,2,1]")
+			},
+		},
+		{
+			"process empty slice",
+			[]interface{}{},
+			func(_ int, elem interface{}) (interface{}, error) {
+				return elem, nil
+			},
+			&testSliceAggregator{state:make([]string, 0)},
+			func(result interface{}, err error) {
+				assert.Nil(t, err)
+				assert.Equal(t, "[]", result.(string))
+			},
+		},
+	}{
+		result, err := WalkSliceInParallel(test.target, test.processFunc, test.aggregator)
+		test.assertion(result, err)
+	}
+}
+
 func TestWalkStringMapInParallel(t *testing.T) {
 	for _, test := range []struct{
 		name 		string
