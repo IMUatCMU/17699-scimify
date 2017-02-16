ae68e6d38cd761e049ee4e3c725dbb6103fa189e
Weinan Qiu <davidiamyou@gmail.com>
2017-02-15 23:24:49 -0500

    Added benchmark for both JSON serializers

---
+++ serialize/default_json_serializer_test.go
@@ -0,0 +1,36 @@
+package serialize
+
+import (
+	"github.com/go-scim/scimify/resource"
+	"testing"
+)
+
+func BenchmarkDefaultJsonSerializer_Serialize(b *testing.B) {
+	// prepare schema
+	schema := &resource.Schema{
+		Schemas:    []string{resource.SchemaUrn},
+		Id:         resource.UserUrn,
+		Name:       "User schema",
+		Attributes: make([]*resource.Attribute, 0),
+	}
+	coreSchema, err := loadSchema("../schemas/common_schema.json")
+	userSchema, err := loadSchema("../schemas/user_schema.json")
+	if err != nil {
+		b.Fatal(err)
+	}
+	schema.MergeWith(coreSchema, userSchema)
+	schema.ConstructAttributeIndex()
+
+	// prepare data
+	resource, _, err := loadResource("../test_data/single_test_user_david.json")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	// serializer
+	b.ResetTimer()
+	serializer := &DefaultJsonSerializer{}
+	for i := 0; i < b.N; i++ {
+		serializer.Serialize(resource, nil)
+	}
+}
--- serialize/schema_json_serializer_test.go
+++ serialize/schema_json_serializer_test.go
@@ -10,6 +10,40 @@ import (
 	"testing"
 )
 
+func BenchmarkSchemaJsonSerializer_Serialize(b *testing.B) {
+	// prepare schema
+	schema := &resource.Schema{
+		Schemas:    []string{resource.SchemaUrn},
+		Id:         resource.UserUrn,
+		Name:       "User schema",
+		Attributes: make([]*resource.Attribute, 0),
+	}
+	coreSchema, err := loadSchema("../schemas/common_schema.json")
+	userSchema, err := loadSchema("../schemas/user_schema.json")
+	if err != nil {
+		b.Fatal(err)
+	}
+	schema.MergeWith(coreSchema, userSchema)
+	schema.ConstructAttributeIndex()
+
+	// prepare data
+	resource, _, err := loadResource("../test_data/single_test_user_david.json")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	// serializer
+	b.ResetTimer()
+	serializer := &SchemaJsonSerializer{}
+	for i := 0; i < b.N; i++ {
+		serializer.Serialize(resource, &SchemaJsonSerializerContext{
+			InclusionPaths: make([]string, 0),
+			ExclusionPaths: make([]string, 0),
+			Schema:         schema,
+		})
+	}
+}
+
 func TestSchemaJsonSerializer_Serialize(t *testing.T) {
 	// prepare schema
 	schema := &resource.Schema{
