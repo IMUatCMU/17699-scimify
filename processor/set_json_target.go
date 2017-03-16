ed76510b874d8dcc9e0dac983dab7296a255c3a8
Weinan Qiu <davidiamyou@gmail.com>
2017-03-16 14:22:21 -0400

    Add JSON target processor

---
+++ processor/set_json_target.go
@@ -0,0 +1,19 @@
+package processor
+
+type setSingleResultAsJsonTargetProcessor struct {}
+
+func (_ *setSingleResultAsJsonTargetProcessor) Process(ctx *ProcessorContext) error {
+	ctx.SerializationTargetFunc = func() interface{} {
+		return ctx.SingleResult
+	}
+	return nil
+}
+
+type setMultipleResultAsJsonTargetProcessor struct {}
+
+func (_ *setMultipleResultAsJsonTargetProcessor) Process(ctx *ProcessorContext) error {
+	ctx.SerializationTargetFunc = func() interface{} {
+		return ctx.MultiResults
+	}
+	return nil
+}
